b214cac64c95487664db5ad0e6c6c2e7928c3de0
Licates <136702730+Licates@users.noreply.github.com>
2025-04-12 03:46:42 +0200

    Improve Dummy Testing

--- src/logic/consistency_checker.py
+++ src/logic/consistency_checker.py
@@ -1,2 +1,4 @@
-def check_consistency(self) -> bool:
-    ...
+from random import randint
+
+def check_consistency(parsed_data) -> bool:
+    return bool(randint(0, 1))  # Randomly returns True or False for testing purposes
