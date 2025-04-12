6262cce29b43b7d8ee2554f25f1d7d140aabd68f
6bf4b307 <6bf4b307>
2025-04-12 03:37:13 +0200

    Add decision maker

--- src/logic.py/consitency_checker.py
+++
@@ -1,9 +0,0 @@
-class ConsistencyChecker:
-    def __init__(self, extracted_data: dict):
-        self.data = extracted_data
-
-    def is_consistent(self) -> bool:
-        ...
-
-    def get_inconsistencies(self) -> list[str]:
-        ...
