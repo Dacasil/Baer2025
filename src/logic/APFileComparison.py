eb86e199236694549400dbb06c51d50730be52d7
Dacasil <173367017+Dacasil@users.noreply.github.com>
2025-04-13 04:42:05 +0200

    text based informations for each option

--- src/logic/APFileComparison.py
+++ src/logic/APFileComparison.py
@@ -6,7 +6,7 @@ from pygments.lexers.oberon import ComponentPascalLexer
 
 
 class Comparison:
-    def __init__(self,account,profile,passport):
+    def __init__(self, account, profile, passport):
         """
         accound: pdf
         description: txt
@@ -14,69 +14,108 @@ class Comparison:
         profile: docx
         """
         date_fields = {
-            'issue_date': '%d%m%Y',
-            'expiration_date': '%d%m%Y',
-            'date_of_birth': '%Y-%m-%d',
-            'ID Issue Date': '%Y-%m-%d',
-            'ID Expiry Date': '%Y-%m-%d'
+            "issue_date": "%d%m%Y",
+            "expiration_date": "%d%m%Y",
+            "date_of_birth": "%Y-%m-%d",
+            "ID Issue Date": "%Y-%m-%d",
+            "ID Expiry Date": "%Y-%m-%d",
         }
 
-
         # png
-        date_of_birth_value = passport.loc[passport['Field'] == 'date_of_birth', 'Value'].iloc[0]
+        date_of_birth_value = passport.loc[
+            passport["Field"] == "date_of_birth", "Value"
+        ].iloc[0]
 
         self.passport = passport
-        self.passport['Field'] = self.passport['Field'].str.lower().str.replace(" ", "", regex=False)
-        #self.passport = passport.set_index("Field").T
+        self.passport["Field"] = (
+            self.passport["Field"].str.lower().str.replace(" ", "", regex=False)
+        )
+        # self.passport = passport.set_index("Field").T
         try:
-            self.passport.loc[self.passport['Field'] == 'date_of_birth', 'Value'] = pd.to_datetime(
-                self.passport.loc[self.passport['Field'] == 'date_of_birth', 'Value'], format='%d%m%Y'
+            self.passport.loc[self.passport["Field"] == "date_of_birth", "Value"] = (
+                pd.to_datetime(
+                    self.passport.loc[
+                        self.passport["Field"] == "date_of_birth", "Value"
+                    ],
+                    format="%d%m%Y",
+                )
             )
         except Exception:
-            self.passport.loc[self.passport['Field'] == 'date_of_birth', 'Value'] = pd.Timestamp('2000-01-01')
+            self.passport.loc[self.passport["Field"] == "date_of_birth", "Value"] = (
+                pd.Timestamp("2000-01-01")
+            )
 
         try:
-            self.passport.loc[self.passport['Field'] == 'issue_date', 'Value'] = pd.to_datetime(
-                self.passport.loc[self.passport['Field'] == 'issue_date', 'Value'], format='%d%m%Y'
+            self.passport.loc[self.passport["Field"] == "issue_date", "Value"] = (
+                pd.to_datetime(
+                    self.passport.loc[self.passport["Field"] == "issue_date", "Value"],
+                    format="%d%m%Y",
+                )
             )
         except Exception:
-            self.passport.loc[self.passport['Field'] == 'issue_date', 'Value'] = pd.Timestamp('2000-01-01')
+            self.passport.loc[self.passport["Field"] == "issue_date", "Value"] = (
+                pd.Timestamp("2000-01-01")
+            )
 
         try:
-            self.passport.loc[self.passport['Field'] == 'expiration_date', 'Value'] = pd.to_datetime(
-                self.passport.loc[self.passport['Field'] == 'expiration_date', 'Value'], format='%d%m%Y'
+            self.passport.loc[self.passport["Field"] == "expiration_date", "Value"] = (
+                pd.to_datetime(
+                    self.passport.loc[
+                        self.passport["Field"] == "expiration_date", "Value"
+                    ],
+                    format="%d%m%Y",
+                )
             )
         except Exception:
-            self.passport.loc[self.passport['Field'] == 'expiration_date', 'Value'] = pd.Timestamp('2000-01-01')
-
+            self.passport.loc[self.passport["Field"] == "expiration_date", "Value"] = (
+                pd.Timestamp("2000-01-01")
+            )
 
         # docx
         self.profile = profile
-        self.profile['Field'] = self.profile['Field'].str.lower().str.replace(" ", "", regex=False)
+        self.profile["Field"] = (
+            self.profile["Field"].str.lower().str.replace(" ", "", regex=False)
+        )
         try:
-            self.profile.loc[self.profile['Field'] == 'dateofbirth', 'Value'] = pd.to_datetime(
-                self.profile.loc[self.profile['Field'] == 'dateofbirth', 'Value'], format='%Y-%m-%d'
+            self.profile.loc[self.profile["Field"] == "dateofbirth", "Value"] = (
+                pd.to_datetime(
+                    self.profile.loc[self.profile["Field"] == "dateofbirth", "Value"],
+                    format="%Y-%m-%d",
+                )
             )
         except Exception:
-            self.profile.loc[self.profile['Field'] == 'dateofbirth', 'Value'] = pd.Timestamp('2000-01-02')
+            self.profile.loc[self.profile["Field"] == "dateofbirth", "Value"] = (
+                pd.Timestamp("2000-01-02")
+            )
 
         try:
-            self.profile.loc[self.profile['Field'] == 'idissuedate', 'Value'] = pd.to_datetime(
-                self.profile.loc[self.profile['Field'] == 'idissuedate', 'Value'], format='%Y-%m-%d'
+            self.profile.loc[self.profile["Field"] == "idissuedate", "Value"] = (
+                pd.to_datetime(
+                    self.profile.loc[self.profile["Field"] == "idissuedate", "Value"],
+                    format="%Y-%m-%d",
+                )
             )
         except Exception:
-            self.profile.loc[self.profile['Field'] == 'idissuedate', 'Value'] = pd.Timestamp('2000-01-02')
+            self.profile.loc[self.profile["Field"] == "idissuedate", "Value"] = (
+                pd.Timestamp("2000-01-02")
+            )
 
         try:
-            self.profile.loc[self.profile['Field'] == 'idexpirydate', 'Value'] = pd.to_datetime(
-                self.profile.loc[self.profile['Field'] == 'idexpirydate', 'Value'], format='%Y-%m-%d'
+            self.profile.loc[self.profile["Field"] == "idexpirydate", "Value"] = (
+                pd.to_datetime(
+                    self.profile.loc[self.profile["Field"] == "idexpirydate", "Value"],
+                    format="%Y-%m-%d",
+                )
             )
         except Exception:
-            self.profile.loc[self.profile['Field'] == 'idexpirydate', 'Value'] = pd.Timestamp('2000-01-02')
-        #self.profile = profile.set_index("Field").T
+            self.profile.loc[self.profile["Field"] == "idexpirydate", "Value"] = (
+                pd.Timestamp("2000-01-02")
+            )
+        # self.profile = profile.set_index("Field").T
         self.account = account
-        self.account['Field'] = self.account['Field'].str.lower().str.replace(" ", "", regex=False)
-
+        self.account["Field"] = (
+            self.account["Field"].str.lower().str.replace(" ", "", regex=False)
+        )
 
     def CrossMatchingProperties(self):
         name = ["name", "First/ Middle Name (s)", "account_holder_name"]
@@ -97,14 +136,20 @@ class Comparison:
         matchesPassport = []
         matchesAccount = []
         for name in nameVersions:
-            matchesProfile.extend(self.profile[self.profile['Field'] == name].values[:, 1])
-            matchesPassport.extend(self.passport[self.passport['Field'] == name].values[:, 1])
-            matchesAccount.extend(self.account[self.account['Field'] == name].values[:, 1])
+            matchesProfile.extend(
+                self.profile[self.profile["Field"] == name].values[:, 1]
+            )
+            matchesPassport.extend(
+                self.passport[self.passport["Field"] == name].values[:, 1]
+            )
+            matchesAccount.extend(
+                self.account[self.account["Field"] == name].values[:, 1]
+            )
 
         matches = {
             "profile": matchesProfile,
             "passport": matchesPassport,
-            "account": matchesAccount
+            "account": matchesAccount,
         }
 
         return matches
@@ -114,18 +159,16 @@ class Comparison:
         nameVersions = [name.lower().replace(" ", "") for name in nameVersions]
         matches = Comparison.findField(self, nameVersions)
 
-        names = {
-            "profile": False,
-            "passport": False,
-            "account": False
-        }
+        names = {"profile": False, "passport": False, "account": False}
         for name1, val1 in matches.items():
             for name2, val2 in matches.items():
                 if name1 == name2:
                     continue
                 for val in val1:
                     try:
-                        if val.lower().replace(" ", "") in [v.lower().replace(" ", "") for v in val2]:
+                        if val.lower().replace(" ", "") in [
+                            v.lower().replace(" ", "") for v in val2
+                        ]:
                             names[name1] = True
                     except:
                         if val in [v for v in val2]:
@@ -138,9 +181,13 @@ class Comparison:
             print(f"====== FALSE ======\n{matches}\n")
         return all_true
 
+
 def comparePrecise(account="", profile="", passport=""):
     Client = Comparison(account, profile, passport)
-    return Client.CrossMatchingProperties()
+    answer = Client.CrossMatchingProperties()
+    print(f"Classic: {answer}")
+    return answer
+
 
 if __name__ == "__main__":
     comparePrecise()
